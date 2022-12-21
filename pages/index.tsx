6e7233cdcbf2213096d7e8ccfd353682a4e6c057
Serey Roth <sereyratanakroth@gmail.com>
2022-12-20 23:17:29 -0800

    update data fetching by querying data from Contentful

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,19 @@
 import Head from 'next/head'
 import Link from 'next/link';
-import { getSortedPostsData } from '../lib/posts';
 
 import Layout from '../components/Layout';
 
+const CONTENTFUL_URL = `https://graphql.contentful.com/content/v1/spaces/${process.env.CONTENTFUL_SPACE_ID}/environments/master`
+
+interface Post {
+    id: string,
+    title: string,
+    firstPublishedAt: string,
+    author: string,
+}
+
 interface HomeProps {
-    posts: [{
-        id: string,
-        title: string,
-        date: string 
-        description: string,
-    }]
+    posts: Post[]
 }
 
 export default function Home({ posts }: HomeProps) {
@@ -22,17 +25,13 @@ export default function Home({ posts }: HomeProps) {
             <section className='flex flex-col gap-2 items-center'>
                 <h1 className='font-bold text-3xl'>Serey's Blog</h1>
                 <div className='flex flex-col gap-2 max-w-[1000px]'>
-                    {posts.map(({ id, title, date, description }) => (
+                    {posts.map(({ id, title, firstPublishedAt, author }) => (
                         <Link key={id} href={`/posts/${id}`}>
                             <div className='border p-2 rounded-lg drop-shadow-sm flex flex-col
                             shadow-sm shadow-gray-100'>
-                                <h3 className='font-semibold'>{title}</h3>
-                                <p className='text-sm mb-2'>{date}</p>
-                                <small>
-                                    <p>
-                                        {description}
-                                    </p>
-                                </small>
+                                <h3 className='font-semibold mb-4'>{title}</h3>
+                                <p className='text-sm'>By: {author}</p>
+                                <p className='text-sm'>Published On: {(new Date(firstPublishedAt)).toDateString()}</p>
                             </div>
                         </Link>
                     ))}
@@ -43,10 +42,45 @@ export default function Home({ posts }: HomeProps) {
 }
 
 export async function getStaticProps() {
-    const allPostsData = getSortedPostsData(); 
-    return { 
+    const collectionQuery = JSON.stringify({
+        query: `{
+            blogPostCollection {
+                items {
+                    sys {
+                        id,
+                        firstPublishedAt,
+                    }
+                    title,
+                    author
+                }
+            }
+        }`
+    });
+
+    const blogEntries = await fetch(
+        CONTENTFUL_URL, 
+        {
+            method: 'POST',
+            body: collectionQuery,
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${process.env.CONTENTFUL_API_KEY}`
+            }
+        }
+    )
+    .then(response => response.json())
+    .then(response => response.data.blogPostCollection.items);
+
+    const posts = blogEntries.map((entry: any) => ({
+        id: entry.sys.id,
+        firstPublishedAt: entry.sys.firstPublishedAt,
+        title: entry.title,
+        author: entry.author,
+    }))
+
+    return {
         props: {
-            posts: allPostsData,
+            posts,
         }
     }
 }
