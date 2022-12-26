ea2b8d9899130a732a423d40afa29ce2ac1205b9
Serey Roth <sereyratanakroth@gmail.com>
2022-12-26 15:09:57 -0800

    change pre-rendering method to server-side for index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,55 +1,11 @@
 import Head from 'next/head';
-import Link from 'next/link';
-import Image from 'next/image';
 
 import Layout from '../components/Layout';
-
-const CONTENTFUL_URL = `https://graphql.contentful.com/content/v1/spaces/${process.env.CONTENTFUL_SPACE_ID}/environments/master`
-
-interface ImageItem {
-    title: string,
-    url: string,
-    description: string,
-}
-
-interface Post {
-    id: string,
-    title: string,
-    firstPublishedAt: string,
-    author: string,
-    images: ImageItem[],
-}
+import Posts from '../components/Posts';
+import { Post, CONTENTFUL_URL } from '../lib';
 
 interface Props {
-    posts: Post[]
-}
-
-const Posts = ({ posts }: Props) => {
-    return (
-        <div className='flex sm:grid sm:grid-cols-2 lg:grid-cols-3 flex-col gap-4 px-2'>
-            {posts.map(({ id, title, firstPublishedAt, author, images }) => (
-                <Link key={id} href={`/posts/${id}`} className='mb-4'>
-                    <div className='flex flex-col gap-2 rounded-xl border relative bg-white drop-shadow-md
-                    h-[350px]'>
-                        <Image
-                            width={250}
-                            height={100}
-                            src={images[0].url}
-                            alt={images[0].description}
-                            className='w-full max-h-[200px] object-cover aspect-square rounded-t-xl'
-                        />
-                        <div className='flex flex-col gap-2 w-full flex-1 p-4'>
-                            <h3 className='font-semibold text-xl lg:text-2xl flex-1 mb-4'>{title}</h3>
-                            <p className='text-sm'>Written by <strong>{author}</strong></p>
-                            <p className='text-sm absolute top-2 invert-[100%] right-4 font-bold'>
-                                {(new Date(firstPublishedAt)).toDateString()}
-                            </p>
-                        </div>
-                    </div>
-                </Link>
-            ))}
-        </div>
-    )
+    posts: Post[];
 }
 
 export default function Home({ posts }: Props) {
@@ -70,7 +26,7 @@ export default function Home({ posts }: Props) {
     )
 }
 
-export async function getStaticProps() {
+export async function getServerSideProps() {
     const collectionQuery = JSON.stringify({
         query: `{
             blogPostCollection {
